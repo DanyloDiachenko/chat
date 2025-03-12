daed7bd725212239ad5bd519c40610eeb0d844f1
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2025-03-12 17:14:05 +0200

    styles

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ const App = () => {
     return (
         <div className="h-screen flex flex-col items-center mt-6">
             <h1 className="text-4xl font-medium container">Chat bots 2.0</h1>
-            <div className="w-full bg-[#586670] mt-3 py-6">
+            <div className="w-full bg-[#586670] min-h-screen mt-3 py-6">
                 <div className="flex container">
                     <div className="w-3/4 border-r flex flex-col">
                         <div className="flex items-center max-h-40">
@@ -27,48 +27,63 @@ const App = () => {
                                 </p>
                             </div>
                         </div>
-                        <div className="flex-1 bg-[#d6dfe7] p-6 rounded-lg overflow-y-auto">
-                            <div className="max-w-4xl">
-                                <div className="rounded-t-md flex items-center justify-between py-2 px-4 bg-[#bdcbd9]">
-                                    <span className="text-gray-700">
-                                        Reverse bot
-                                    </span>
-                                    <span className="text-gray-400">
-                                        4:20 PM
-                                    </span>
+                        <div className="bg-[#d6dfe7] p-6">
+                            <div className="flex-1 overflow-y-auto">
+                                <div className="max-w-4xl">
+                                    <div className="rounded-t-md flex items-center justify-between py-2 px-4 bg-[#bdcbd9]">
+                                        <span className="text-gray-700">
+                                            Reverse bot
+                                        </span>
+                                        <span className="text-gray-400">
+                                            4:20 PM
+                                        </span>
+                                    </div>
+                                    <div className="px-4 py-2 bg-white shadow-lg text-gray-700 rounded-b-md relative">
+                                        <p>Hello world!</p>
+                                        <div className="absolute -left-2 top-1/2 -translate-y-1/2 bg-white w-4 h-4 rotate-45"></div>
+                                    </div>
                                 </div>
-                                <div className="px-4 py-2 bg-white shadow-lg text-gray-700 rounded-b-md relative">
-                                    <p>Hello world!</p>
-                                    <div className="absolute -left-2 top-1/2 -translate-y-1/2 bg-white w-4 h-4 rotate-45"></div>
-                                </div>
-                            </div>
-                            <div className="max-w-4xl mt-5 ml-auto">
-                                <div className="rounded-t-md flex items-center justify-between py-2 px-4 bg-[#f0cbb3]">
-                                    <span className="text-gray-700">
-                                        Username
-                                    </span>
-                                    <span className="text-gray-400">
-                                        4:22 PM
-                                    </span>
-                                </div>
-                                <div className="px-4 py-2 bg-white shadow-lg text-gray-700 rounded-b-md relative">
-                                    <p>Hello robot!</p>
-                                    <div className="absolute -right-2 top-1/2 -translate-y-1/2 bg-white w-4 h-4 rotate-45"></div>
-                                    <div className="absolute -bottom-6 text-sm text-gray-400 z-10">
-                                        Seen 4:27 PM
+                                <div className="max-w-4xl mt-5 ml-auto">
+                                    <div className="rounded-t-md flex items-center justify-between py-2 px-4 bg-[#f0cbb3]">
+                                        <span className="text-gray-700">
+                                            Username
+                                        </span>
+                                        <span className="text-gray-400">
+                                            4:22 PM
+                                        </span>
+                                    </div>
+                                    <div className="px-4 py-2 bg-white shadow-lg text-gray-700 rounded-b-md relative">
+                                        <p>Hello robot!</p>
+                                        <div className="absolute -right-2 top-1/2 -translate-y-1/2 bg-white w-4 h-4 rotate-45"></div>
+                                        <div className="absolute -bottom-6 text-sm text-gray-400 z-10">
+                                            Seen 4:27 PM
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <div className="mt-4">
-                            <input
-                                type="text"
-                                placeholder="Start chatting!"
-                                className="w-full p-2 border rounded-lg"
-                            />
-                            <button className="mt-2 w-full bg-blue-500 text-white py-2 rounded-lg">
-                                Send message
-                            </button>
+                            <div className="text-center text-[#84aec6] mt-6">
+                                Reverse bot is typing...
+                            </div>
+                            <form className="mt-4 flex w-full gap-3">
+                                <input
+                                    type="text"
+                                    className="max-w-[75%] w-full rounded-md px-3 focus:outline-blue-300 outline-2 text-gray-700"
+                                    placeholder="Start chatting!"
+                                />
+                                <button className="max-w-[25%] w-full bg-[#428bca] text-white py-3 rounded-md text-opacity-80">
+                                    Send message
+                                </button>
+                            </form>
+                            {/* <div className="mt-4">
+                                <input
+                                    type="text"
+                                    placeholder="Start chatting!"
+                                    className="w-full p-2 border rounded-lg"
+                                />
+                                <button className="mt-2 w-full bg-blue-500 text-white py-2 rounded-lg">
+                                    Send message
+                                </button>
+                            </div> */}
                         </div>
                     </div>
                     {/* Right Panel - Bot List */}
